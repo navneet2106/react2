f59d104f37bf74ab1a23722242e153ac069a11d0
navneet2106 <navneetbhardwaj2106@gmail.com>
2020-05-24 21:47:14 +0530

    First commit in react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcom to my first program. Navneet Bhardwaj
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          yeo
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
